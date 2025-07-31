af370eda7e8f7ccd7ab199af5fed4531c7bb79f2
Yurii Filimonchuk <filimon4uck.yura@gmail.com>
2025-07-31 12:12:42 +0200

    Added rds and aurora cluster

---
+++ lesson-db-module/modules/s3-backend/dynamodb.tf
@@ -0,0 +1,17 @@
+resource "aws_dynamodb_table" "terraform_locks" {
+    name         = var.table_name
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "LockID"
+
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+
+
+    tags = {
+        NAME        = "Terraform Lock Table"
+        Environment = "lesson-5"
+    }
+}
